565c36bd5c52a20022a7438ac7851e1b83f1d689
gilkorni <kornitzergil@gmail.com>
2023-05-16 12:24:18 +0300

    15/5/23 12:24

--- PieceOfCar/search.js
+++ PieceOfCar/search.js
@@ -34,3 +34,12 @@ function closeModal(modal) {
     modal.classList.remove('active')
     overlay.classList.remove('active')
 }
+
+const iconDown1 = document.querySelector(".iconDown1");
+  iconDown1.addEventListener("click", () => {
+    window.location.href = "contact.html";
+  });
+    const iconDown2 = document.querySelector(".iconDown2");
+  iconDown2.addEventListener("click", () => {
+    window.location.href = "contact.html";
+  });
